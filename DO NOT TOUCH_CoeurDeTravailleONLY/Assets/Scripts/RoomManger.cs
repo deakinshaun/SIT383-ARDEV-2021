e850c367537dcdd9c47ace4b41cedc3ad41990b6
Ken Lau <ksla@deakin.edu.au>
2021-05-01 16:24:09 +1000

    Import Join Room Button

---
+++ DO NOT TOUCH_CoeurDeTravailleONLY/Assets/Scripts/RoomManger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManger : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Login");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We have connected");
+        RoomOptions roomopt = new RoomOptions();
+        PhotonNetwork.JoinOrCreateRoom("AppRoom", roomopt, new TypedLobby("AppLobby", LobbyType.Default));
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("You are in a room with  " + PhotonNetwork.CurrentRoom.PlayerCount + "  Other Players.");
+
+    }
+}
