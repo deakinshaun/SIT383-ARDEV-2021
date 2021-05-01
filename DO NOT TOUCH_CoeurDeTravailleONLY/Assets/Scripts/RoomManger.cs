b5011c14e89deddc18efbf0f45f0b1687cfa7617
Ken Lau <ksla@deakin.edu.au>
2021-05-01 16:41:29 +1000

    Clock and Countdown are imported

--- DO NOT TOUCH_CoeurDeTravailleONLY/Assets/Scripts/RoomManger.cs
+++ DO NOT TOUCH_CoeurDeTravailleONLY/Assets/Scripts/RoomManger.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Photon.Pun;
 using Photon.Realtime;
 
 public class RoomManger : MonoBehaviourPunCallbacks
 {
+
+    public Text TimerDisplay;
+    private float countDownValue = 60f * 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +25,20 @@ public class RoomManger : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinOrCreateRoom("AppRoom", roomopt, new TypedLobby("AppLobby", LobbyType.Default));
     }
 
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("You are in a room with  " + PhotonNetwork.CurrentRoom.PlayerCount + "  Other Players.");
+        
+    }
+
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        countDownValue -= Time.deltaTime;
+        TimerDisplay.text = string.Format("{0:00}:{1:00}", ((int)(countDownValue / 60) % 60).ToString("d2"), ((int)(countDownValue % 60)).ToString("d2"));
     }
 
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("You are in a room with  " + PhotonNetwork.CurrentRoom.PlayerCount + "  Other Players.");
 
-    }
 }
