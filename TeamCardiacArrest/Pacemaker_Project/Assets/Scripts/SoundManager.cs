2c3ce9f126dda7af81dbb6beba1e16d6876ee1e7
Aya <64139634+Pixelmixery@users.noreply.github.com>
2021-05-05 18:29:05 +1000

    Revert "Figuring out Photon Voice 2"
    This reverts commit 17c682f76791ec8a509a2c41c6ee6484333e0a29.

--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public float distance = 20.0f;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,15 +22,7 @@ public class SoundManager : MonoBehaviour
     }
     public static void hospitalTrolley()
     {
-        //
-        float x = -10;
-        float z = 2;
-        Instantiate(TrolleyWaker,Vector3(x,-2,z));
-        while (x<10)
-        {
-        x = distance * Time.time;
-        transform position = new Vector3(transform.position.x);
-        }
+        
     }
     public static void manInPain()
     {
