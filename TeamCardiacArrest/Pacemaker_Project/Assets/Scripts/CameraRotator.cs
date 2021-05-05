17c682f76791ec8a509a2c41c6ee6484333e0a29
Aya <64139634+Pixelmixery@users.noreply.github.com>
2021-05-05 18:20:41 +1000

    Figuring out Photon Voice 2

---
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+            transform.rotation = 
+            Quaternion.Euler (90, 0, 90) * 
+            Input.gyro.attitude * 
+            Quaternion.Euler (180, 180, 0);
+        }        
+    }
+}
--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public float distance = 20.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +24,15 @@ public class SoundManager : MonoBehaviour
     }
     public static void hospitalTrolley()
     {
-        
+        //
+        float x = -10;
+        float z = 2;
+        Instantiate(TrolleyWaker,Vector3(x,-2,z));
+        while (x<10)
+        {
+        x = distance * Time.time;
+        transform position = new Vector3(transform.position.x);
+        }
     }
     public static void manInPain()
     {
