5034fc89331d0bbf40fe4b8ad615e27bb0618156
Aya <pixelmixery@gmail.com>
2021-05-05 17:22:58 +1000

    Added Spatial and Mutual Presence Module
    Added 7 sounds with public script (Yes, Shaun, Week 8 did help with that!), Added Panoramic cube for immersion experience, Prefabs for mutual presence

---
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void ivMachine()
+    {
+        
+    }
+    public static void hospitalTrolley()
+    {
+        
+    }
+    public static void manInPain()
+    {
+        
+    }
+    public static void HospitalQuiet()
+    {
+        
+    }
+    public static void HospitalLoud()
+    {
+        
+    }
+    public static void beep()
+    {
+        
+    }
+    public static void flatLine()
+    {
+        
+    }
+
+}
