5462435121f779df85a6e5bf09ab5aba36303424
mahanq <37131354+mahanq@users.noreply.github.com>
2021-05-08 13:41:43 +0800

    Final Prototype
    This is the final prototype which is contain the final prototype of the text message which allow user to type the text and send it out.

---
+++ Go-For-It Final Prototype/Assets/Scripts/Rope.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public Transform endTarget;
+    public float ropeThickness = 0.1f;
+    Vector3 startPoint;
+    Vector3 endPoint;
+    float distance = 0f;
+    void Start()
+    {
+        startPoint = transform.position;
+        transform.localScale = Vector3.one * ropeThickness;
+    }
+
+    void Update()
+    {
+        endPoint = endTarget.transform.position;
+        distance = Vector3.Distance(endPoint, startPoint);
+        transform.position = (startPoint + endPoint) / 2f;
+        Vector3 loc = transform.localScale;
+        loc.y = distance / 2f;
+        transform.localScale = loc;
+        //transform.rotation = Quaternion.LookRotation(Vector3.forward, );
+        //transform.LookAt(endTarget, transform.position- endPoint);
+
+        transform.LookAt(endTarget.transform.position);
+        transform.Rotate(Vector3.right * 90f);
+    }
+}
