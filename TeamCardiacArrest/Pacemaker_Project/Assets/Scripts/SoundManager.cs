e79d87dcb14293127928816539071b44831e2624
Aya <64139634+Pixelmixery@users.noreply.github.com>
2021-05-09 14:15:12 +1000

    Sound Manager Script Update, attached sounds to prefab

--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/SoundManager.cs
@@ -4,45 +4,91 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    
+    private float distance = 3.0f;
+    private float frequency = 0.3f;
+    private float decayFactor = 1.0f;
+    private float dropoff = 0.1f;
+    private List<AudioSource> sounds = new List<AudioSource>();
+
+
+    private AudioSource ss_trolley;
+    private AudioSource ss_ivMachine;
+    private AudioSource ss_manInPain;
+    private AudioSource ss_hospitalLoud;
+    private AudioSource ss_hospitalQuiet;
+    private AudioSource ss_beep;
+    private AudioSource ss_flatline;
+
+
+    
+    public GameObject listener;
+
+    public GameObject ss_trolleyObject;
+    public GameObject ss_ivMachineObject;
+    public GameObject ss_manInPainObject;
+    public GameObject ss_hospitalLoudObject;
+    public GameObject ss_hospitalQuietObject;
+    public GameObject ss_beepObject;
+    public GameObject ss_flatlineObject;
+ 
+    
     // Start is called before the first frame update
+
     void Start()
-    {
-        
+    {    
+        sounds.Add(ss_ivMachine = ss_ivMachineObject.GetComponent<AudioSource>());
+        sounds.Add(ss_trolley = ss_trolleyObject.GetComponent<AudioSource>());
+        sounds.Add(ss_manInPain = ss_manInPainObject.GetComponent<AudioSource>());
+        sounds.Add(ss_hospitalLoud = ss_hospitalLoudObject.GetComponent<AudioSource>());
+        sounds.Add(ss_hospitalQuiet = ss_hospitalQuietObject.GetComponent<AudioSource>());
+        sounds.Add(ss_beep = ss_beepObject.GetComponent<AudioSource>());
+        sounds.Add(ss_flatline = ss_flatlineObject.GetComponent<AudioSource>());
+        Debug.Log("Sound loaded:" + ss_ivMachine);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public static void ivMachine()
+    public void ivMachine()
     {
-        
+        float distanceIv = (ss_ivMachine.transform.position - listener.transform.position).magnitude;
+        ivMachine.volume = 1.0f / Mathf.Pow (distanceIv, decayFactor);
+        ss_ivMachine.Play();
     }
-    public static void hospitalTrolley()
+    public void hospitalTrolley()
     {
-        
+        ss_trolley.Play();        
     }
-    public static void manInPain()
+    public void manInPain()
     {
-        
+        ss_manInPain.Play();    
     }
-    public static void HospitalQuiet()
+    public void HospitalLoud()
     {
-        
+        ss_hospitalLoud.Play();        
     }
-    public static void HospitalLoud()
+    public void HospitalQuiet()
     {
-        
+        ss_hospitalQuiet.Play();
     }
-    public static void beep()
+    public void beep()
     {
-        
+        ss_beep.Play();
     }
-    public static void flatLine()
+    public void flatLine()
     {
-        
+        ss_flatline.Play();
     }
-
+    public void EndSimulation()
+    {
+        for (int i = 0; i < sounds.Count; i++)
+        {
+            sounds[i].Stop();
+        }
+    }
 }
