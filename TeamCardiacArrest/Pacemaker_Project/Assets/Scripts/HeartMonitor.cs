dda7b03b26b90f5797211bbaa87644b6e7bd9498
Phillip G <phillip.georgoudakis@gmail.com>
2021-05-13 18:48:57 +1000

    Updated Heart monitor scripts Created bar prefab accidentally deleted

--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/HeartMonitor.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/HeartMonitor.cs
@@ -5,22 +5,23 @@ using UnityEngine.UI;
 
 public class HeartMonitor : MonoBehaviour
 {
-    public Text DebugText;
     //List of spectrum values
     private List<double> timeSeries = new List<double>();
     private List<double> savedSeries = new List<double>();
     //Number of sensor readings
     private int numReading = 256;
 
-    private float width = 4.0f; //Width of spectrum chart
-    private float timeOffset = -2.0f;  //Offset to center of the spectrum chart
+    private float width = 6.0f; //Width of spectrum chart
+    private float timeOffset = 0.0f;  //Offset t o center of the spectrum chart
 
     private int timeStep = 4; //Number of adjacent bins that are condensed into one
-    private float smoothedPeak = 0.0f; //Smoothed value for the peak measured at each frame
-    private float smoothingFactor = 0.98f; // Proportion of the smoothed value retained on each frame.
+    public float smoothedPeak = 0.0f; //Smoothed value for the peak measured at each frame
+    public float smoothingFactor = 0.98f; // Proportion of the smoothed value retained on each frame.
 
     private float minFreq = 0.50f;
-    private float maxFreq = 3.20f;
+    private float maxFreq = 3.90f;
+
+    public int BPM;
 
     public GameObject timeSeriesParent;
     public GameObject frequencySeriesParent;
@@ -34,36 +35,27 @@ public class HeartMonitor : MonoBehaviour
     private bool enter;
     private bool playRecording;
 
-    private void drawChart(double [] values, GameObject parent)
+    void CreateBeat()
+	{
+	}
+
+    private void drawChart(List<double> values, GameObject parent)
     {
         //Clear spectrum chart
         foreach (Transform child in parent.transform)
         {
             Destroy(child.gameObject);
         }
-        for (int i = 0; i < values.Length / 2; i+= timeStep)
+        for (int i = 0; i < values.Count / 2; i+= timeStep)
         {
             //Spectrum width (How long it is)
             GameObject g = Instantiate(barTemplate);
             g.transform.position = new Vector3(width * (float) i / (float) (numReading / 2) + timeOffset, 0, 0);
             g.transform.localScale = new Vector3(width / (float) (numReading / (2 * timeStep)), 
-                                                (float) values[i] / 2,
+                                                0.5f * (float) values[i],
                                                 width / (float)(numReading / (2 * timeStep)));
             g.transform.SetParent(parent.transform, false);
         }
-    }
-
-    //Overload for drawChar to convert saved list to an array (Check)
-    private void drawChart(List<double> values, GameObject parent)
-	{
-        double[] savedSeries = new double[numReading];
-        
-        for (int i = 0; i < numReading; i++)
-        {
-            savedSeries[i] = timeSeries[i];
-        }
-
-        drawChart(savedSeries, parent);
     }
 
     //Saves current view of the frequency for later use.
@@ -72,7 +64,6 @@ public class HeartMonitor : MonoBehaviour
         debugText.text = "Current heartrate view saved";
         toggleButton.interactable = true;
 
-        //double[] savedSeries = new double[numReading];
 
         for (int i = 0; i < numReading; i++)
 		{
@@ -80,7 +71,6 @@ public class HeartMonitor : MonoBehaviour
 		}
 
         drawChart(savedSeries, frequencySeriesParent);
-        wait(2.0f);
     }
 
     //Toggle the playRecording boolean and (change the play button text )
@@ -91,12 +81,19 @@ public class HeartMonitor : MonoBehaviour
         if (playRecording)
         {
             debugText.text = "Recording playing now";
+            enter = true;
             toggleText.text = "Stop";
             recordButton.interactable = false;
+
+            for (int i = 0; i < numReading; i++)
+            {
+                savedSeries[i] = timeSeries[i];
+            }
         }
         else
         { 
-            debugText.text = "";
+            debugText.text = "Recording stopping";
+            enter = true;
             toggleText.text = "Play";
             recordButton.interactable = true;
         }
@@ -107,15 +104,19 @@ public class HeartMonitor : MonoBehaviour
 	{
         savedSeries.Clear();
         debugText.text = "Saved cleared";
+        enter = true;
         toggleButton.interactable = false;
-        wait(2.0f);
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(Display(2.0f));
     }
 
     void FixedUpdate()
     {
         //Sample acceleration
         Vector3 acc = Input.acceleration;
-        //DebugText.text = "Acc: " + acc;
         timeSeries.Add(acc.magnitude);
 
         while (timeSeries.Count > numReading)
@@ -126,8 +127,7 @@ public class HeartMonitor : MonoBehaviour
         {
             if (!playRecording)
             {
-                DebugText.text = "Readings sufficient";
-                //drawChart(timeSeries, timeSeriesParent);
+
                 double[] seriesReal = new double[numReading];
                 double[] seriesComplex = new double[numReading];
                 for (int i = 0; i < numReading; i++)
@@ -139,16 +139,6 @@ public class HeartMonitor : MonoBehaviour
                 int m = (int)(Mathf.Log(numReading) / Mathf.Log(2.0f));
                 //Transform sensor data into frequency values.
                 FFTLibrary.Complex.FFT(1, m, seriesReal, seriesComplex);
-                
-                /*
-                //Get magnitute of complex number (squared)
-                double[] seriesMagnitude = new double[numReading];
-                for (int i = 0; i < numReading; i++)
-                {
-                    seriesMagnitude[i] = 1000.0 * (seriesReal[i] * seriesReal[i] + seriesComplex[i] * seriesComplex[i]);
-                }
-                //drawChart(seriesMagnitude, frequencySeriesParent);
-                */
 
                 //Sensor sample rate.
                 float baseFreq = 1.0f / Time.deltaTime;
@@ -169,7 +159,7 @@ public class HeartMonitor : MonoBehaviour
                         float freq = index * binFreq;
                         if ((freq >= minFreq) && (freq <= maxFreq))
 						{
-                            if((!maxFound) && (maxLevel < v))
+                            if((!maxFound) || (maxLevel < v))
 							{
                                 maxLevel = v;
                                 maxFound = true;
@@ -177,18 +167,18 @@ public class HeartMonitor : MonoBehaviour
 							}
 						}
                     }
-                    smoothedPeak = smoothingFactor * smoothedPeak + (1.0f - smoothingFactor) * peakFreq;
-                    drawChart(seriesReal, timeSeriesParent);
-                    
-                    if (!enter) debugText.text = (smoothedPeak * 60) + " bpm";
-                }   
-                
+                }
+                BPM = (int)Mathf.RoundToInt(smoothedPeak * 60); // Calculate BPM (Beats per second * 60 seconds)
+                smoothedPeak = smoothingFactor * smoothedPeak + (1.0f - smoothingFactor) * peakFreq;
+             
+                drawChart(timeSeries, timeSeriesParent);
             }
 
-			else
+            else
 			{
                 //Play recording that's saved
-                drawChart(savedSeries, timeSeriesParent);
+                InvokeRepeating("drawChart", 1.0f, 0.5f);
+                //drawChart(savedSeries, timeSeriesParent);
 			}
         }
         else
@@ -197,11 +187,14 @@ public class HeartMonitor : MonoBehaviour
         }
     }
 
-    IEnumerator wait(float time)
+    IEnumerator Display(float time)
     {
-        enter = true;
-        yield return new WaitForSeconds(time);
-        enter = false;
+        while (true)
+        {
+            if (!enter) debugText.text = BPM.ToString() + " bpm";
+            yield return new WaitForSeconds(time);
+            enter = false;
+        }
     }
 
 }
