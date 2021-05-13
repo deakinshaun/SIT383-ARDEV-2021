afe77c79b8e7e0e41b383ddfeee3ec8e47c187f7
Thomas Laing <thomaslaing50@gmail.com>
2021-05-13 17:28:18 +1000

    AtrialDeficit - Movement Speed added to MoveAvatar

--- TeamAtrialDeficit/Assets/Scripts/PortalScripts/MoveAvatar.cs
+++ TeamAtrialDeficit/Assets/Scripts/PortalScripts/MoveAvatar.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MoveAvatar : MonoBehaviour
 {
+    public float movementSpeed = 0.2f;
+    public float rotationSpeed = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,7 @@ public class MoveAvatar : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        transform.position += v * transform.forward * Time.deltaTime * 100.0f;
-        transform.rotation *= Quaternion.AngleAxis(h * 1000.0f * Time.deltaTime, transform.up);
+        transform.position += v * transform.forward * Time.deltaTime * 100.0f * movementSpeed;
+        transform.rotation *= Quaternion.AngleAxis(h * 1000.0f * Time.deltaTime * rotationSpeed, transform.up);
     }
 }
