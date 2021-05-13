79faf4f1ecb0563c49ee1e2c12ce6ad02c8006fd
James Bolitho <jabolitho@deakin.edu.au>
2021-05-13 17:09:47 +1000

    Added basic speech recognition + replaced virtual environments with 360 hospital captures, portals now lead to real hospital environments

--- TeamAtrialDeficit/Assets/Scripts/PortalScripts/ChangeUniverse.cs
+++ TeamAtrialDeficit/Assets/Scripts/PortalScripts/ChangeUniverse.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class ChangeUniverse : MonoBehaviour
 {
-    public Material phyCamMaterial;
     public Material virCamMaterial;
     public Material virCamMaterial2;
     public Material virCamMaterial3;
-    public Material virCamMaterial4;
     public GameObject portal;
     public GameObject portal2;
     public GameObject portal3;
@@ -17,15 +15,13 @@ public class ChangeUniverse : MonoBehaviour
     public GameObject virtualview3;
 
     public bool inPhysical = true;
-    //public bool inPhysical2 = true;
-    //public bool inPhysical3 = true;
 
     public GameObject soundManager; //Brendan: Added so portal sound can be played when user enters.
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inPhysical = true;
     }
 
     // Update is called once per frame
@@ -33,41 +29,16 @@ public class ChangeUniverse : MonoBehaviour
     {
         if (inPhysical)
         {
-            phyCamMaterial.SetInt("_Stencil_Level", 0);
             virCamMaterial.SetInt("_Stencil_Level", 1);
             virCamMaterial2.SetInt("_Stencil_Level", 2);
             virCamMaterial3.SetInt("_Stencil_Level", 3);
         }
         else
         {
-            phyCamMaterial.SetInt("_Stencil_Level", 1);
             virCamMaterial.SetInt("_Stencil_Level", 0);
             virCamMaterial2.SetInt("_Stencil_Level", 0);
             virCamMaterial3.SetInt("_Stencil_Level", 0);
-            //virCamMaterial.SetInt("_Stencil_Level", 0);
-            //virCamMaterial2.SetInt("_Stencil_Level", 0);
-            //virCamMaterial3.SetInt("_Stencil_Level", 0);
         }
-        /*if (inPhysical2)
-        {
-            phyCamMaterial.SetInt("_Stencil_Level", 0);
-            virCamMaterial2.SetInt("_Stencil_Level", 2);
-        }
-        else
-        {
-            phyCamMaterial.SetInt("_Stencil_Level", 2);
-            virCamMaterial2.SetInt("_Stencil_Level", 0);
-        }
-        if (inPhysical3)
-        {
-            phyCamMaterial.SetInt("_Stencil_Level", 0);
-            virCamMaterial3.SetInt("_Stencil_Level", 3);
-        }
-        else
-        {
-            phyCamMaterial.SetInt("_Stencil_Level", 3);
-            virCamMaterial3.SetInt("_Stencil_Level", 0);
-        }*/
     }
     public void OnTriggerEnter(Collider other)
     {
@@ -75,23 +46,17 @@ public class ChangeUniverse : MonoBehaviour
         {
             Debug.Log("Changing Universe");
             inPhysical = !inPhysical;
-            //inPhysical2 = !inPhysical2;
-            //inPhysical3 = !inPhysical3;
             soundManager.GetComponent<SoundManager>().advancedPortalSoundPlay(); //Brendan: Plays portal sound
         }
         if (other.gameObject == portal2)
         {
             Debug.Log("Changing Universe2");
-            //inPhysical2 = !inPhysical2;
             inPhysical = !inPhysical;
-            //inPhysical3 = !inPhysical3;
             soundManager.GetComponent<SoundManager>().intermediatePortalSoundPlay(); //Brendan: Plays portal sound
         }
         if (other.gameObject == portal3)
         {
             Debug.Log("Changing Universe3");
-            //inPhysical3 = !inPhysical3;
-            //inPhysical2 = !inPhysical2;
             inPhysical = !inPhysical;
             soundManager.GetComponent<SoundManager>().beginnerPortalSoundPlay(); //Brendan: Plays portal sound
         }
---
+++ TeamAtrialDeficit/Assets/Scripts/SpeechRecScripts/SpeechRecA.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.IO;
+using System;
+
+public class SpeechRecA : MonoBehaviour
+{
+    public Text outputText;
+
+    //This will need to be replaced with your own key.
+    private string subscriptionKey = "21e25a398508422c9b721319afefd550";
+    private string token;
+    //length of any recording sent. 10s is the current limit.
+    public int recordDuration = 5;
+
+    private static bool TrustCertificate(object sender, X509Certificate x509Certificate, X509Chain x509Chain, SslPolicyErrors sslPolicyErrors)
+    {
+        return true;
+    }
+    private void Start()
+    {
+        ServicePointManager.ServerCertificateValidationCallback = TrustCertificate;
+    }
+
+    //These structures contain the relevant fields from the response string/JSON that is returned by the service
+    [System.Serializable]
+    class AssemblyResponse
+    {
+        public string id;
+        public string status;
+        public string text;
+    };
+    [System.Serializable]
+    class AssemblyUploadResponse
+    {
+        //public string upload url;
+    };
+}
