b6fac70ce83f1f6cb9dde1fb6b3f96301b7fcd09
Aya <64139634+Pixelmixery@users.noreply.github.com>
2021-05-13 13:00:49 +1000

    Ar Core deleted and 360 Lobby Prefabified

--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/PhotonManager.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/PhotonManager.cs
@@ -27,24 +27,42 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinOrCreateRoom("ApplicationRoom", roomopt, new TypedLobby("ApplicationLobby", LobbyType.Default));
     }
     // Update is called once per frame
+
+
+    // AR core card on the floor create environment, simulation
+    // !Surface based generation
+    // marker or placed plane
+
+    //Lobby and room abstraction
+    // !two boxes in one scene
+    // !same photon room
+
+    //Room a or b
+    // Solo run
+    // mentor and nurses
     public override void OnJoinedRoom()
     {
         switch (roomtype)
         {
             case 1:
                 PhotonNetwork.Instantiate(SetupPrefab.name, new Vector3(), Quaternion.identity, 0);
+                //get camera componenet, look though
                 message.text = "Just you on this run";
                 Debug.Log("Just you on this run");
                 break;            
 
             case 2:
                 PhotonNetwork.Instantiate(SetupPrefab.name, new Vector3(), Quaternion.identity, 0);
+                //get camera componenet, look though
+                //camera inactive on later intances
                 message.text = "You are in the room with " + PhotonNetwork.CurrentRoom.PlayerCount + " other people as a nurse";
                 Debug.Log("You are in the room with " + PhotonNetwork.CurrentRoom.PlayerCount + " other people as a nurse");
                 break;            
 
             case 3:
                 PhotonNetwork.Instantiate(TeacherPrefab.name, new Vector3(), Quaternion.identity, 0);
+                //get camera componenet, look though
+                //camera inactive on later intances
                 message.text = "You are in the room with " + PhotonNetwork.CurrentRoom.PlayerCount + " other people as a mentor";
                 Debug.Log("You are in the room with " + PhotonNetwork.CurrentRoom.PlayerCount + " other people as a mentor");
                 break;            
