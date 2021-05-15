ba8afb11ecf0de49112656a23b92bf68d30e6b2e
James Bolitho <jabolitho@deakin.edu.au>
2021-05-15 13:07:37 +1000

    Portals lead to each scene, only one available in scene + monitor and bed proper position s2

--- TeamAtrialDeficit/Assets/Scripts/PortalScripts/ChangeUniverse.cs
+++ TeamAtrialDeficit/Assets/Scripts/PortalScripts/ChangeUniverse.cs
@@ -10,13 +10,16 @@ public class ChangeUniverse : MonoBehaviour
     public GameObject portal;
     public GameObject portal2;
     public GameObject portal3;
-    public GameObject virtualview1;
-    public GameObject virtualview2;
-    public GameObject virtualview3;
     public GameObject monitor;
     public GameObject bed;
+    public GameObject VirtualCameraView;
+    public GameObject VirtualCameraView2;
+    public GameObject VirtualCameraView3;
 
     public bool inPhysical = true;
+    public bool inVirtual = false;
+    public bool inVirtual2 = false;
+    public bool inVirtual3 = false;
 
     public GameObject soundManager; //Brendan: Added so portal sound can be played when user enters.
 
@@ -24,6 +27,7 @@ public class ChangeUniverse : MonoBehaviour
     void Start()
     {
         inPhysical = true;
+
     }
 
     // Update is called once per frame
@@ -34,12 +38,42 @@ public class ChangeUniverse : MonoBehaviour
             virCamMaterial.SetInt("_Stencil_Level", 1);
             virCamMaterial2.SetInt("_Stencil_Level", 2);
             virCamMaterial3.SetInt("_Stencil_Level", 3);
+            inVirtual3 = false;
+            inVirtual = false;
+            inVirtual2 = false;
+            portal.SetActive(true);
+            portal2.SetActive(true);
+            portal3.SetActive(true);
+            VirtualCameraView.SetActive(true);
+            VirtualCameraView2.SetActive(true);
+            VirtualCameraView3.SetActive(true);
         }
         else
         {
             virCamMaterial.SetInt("_Stencil_Level", 0);
             virCamMaterial2.SetInt("_Stencil_Level", 0);
             virCamMaterial3.SetInt("_Stencil_Level", 0);
+        }
+        if (inVirtual3)
+        {
+            portal2.SetActive(false);
+            portal3.SetActive(false);
+            VirtualCameraView.SetActive(false);
+            VirtualCameraView2.SetActive(false);
+        }
+        if (inVirtual2)
+        {
+            portal.SetActive(false);
+            portal3.SetActive(false);
+            VirtualCameraView.SetActive(false);
+            VirtualCameraView3.SetActive(false);
+        }
+        if (inVirtual)
+        {
+            portal2.SetActive(false);
+            portal.SetActive(false);
+            VirtualCameraView2.SetActive(false);
+            VirtualCameraView3.SetActive(false);
         }
     }
     public void OnTriggerEnter(Collider other)
@@ -48,6 +82,7 @@ public class ChangeUniverse : MonoBehaviour
         {
             Debug.Log("Changing Universe");
             inPhysical = !inPhysical;
+            inVirtual3 = true;
             soundManager.GetComponent<SoundManager>().advancedPortalSoundPlay(); //Brendan: Plays portal sound
             monitor.layer = 12;
             foreach (GameObject child in monitor.transform)
@@ -58,8 +93,9 @@ public class ChangeUniverse : MonoBehaviour
         }
         if (other.gameObject == portal2)
         {
-            Debug.Log("Changing Universe2");
             inPhysical = !inPhysical;
+            inVirtual2 = true;
+            Debug.Log("Changing Universe2");
             soundManager.GetComponent<SoundManager>().intermediatePortalSoundPlay(); //Brendan: Plays portal sound
             monitor.layer = 11;
             foreach (GameObject child in monitor.transform)
@@ -70,8 +106,9 @@ public class ChangeUniverse : MonoBehaviour
         }
         if (other.gameObject == portal3)
         {
-            Debug.Log("Changing Universe3");
             inPhysical = !inPhysical;
+            inVirtual = true;
+            Debug.Log("Changing Universe3");
             soundManager.GetComponent<SoundManager>().beginnerPortalSoundPlay(); //Brendan: Plays portal sound
             monitor.layer = 10;
             foreach (GameObject child in monitor.transform)
