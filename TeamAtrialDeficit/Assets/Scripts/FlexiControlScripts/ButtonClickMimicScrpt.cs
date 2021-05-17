e75b589b4e2cd4472764cf2b1c64b7bd15d28f12
Rahul Edirisinghe <redirisinghe@deakin.edu.au>
2021-05-17 18:45:24 +1000

    Some fixes and code changes

--- TeamAtrialDeficit/Assets/Scripts/FlexiControlScripts/ButtonClickMimicScrpt.cs
+++ TeamAtrialDeficit/Assets/Scripts/FlexiControlScripts/ButtonClickMimicScrpt.cs
@@ -8,8 +8,8 @@ public class ButtonClickMimicScrpt : MonoBehaviour
     public GameObject Monitor1;
     public GameObject Monitor2;
     public GameObject Monitor3;
-    public string Direction; //up or down
-    
+    public string Direction; 
+    public GameObject SoundManager;
 
 
     // Start is called before the first frame update
@@ -20,6 +20,8 @@ public class ButtonClickMimicScrpt : MonoBehaviour
 
     public void WhenPressed()
     {
+        SoundManager.GetComponent<SoundManager>().button1SoundPlay();
+
         Monitor1.GetComponent<MonitorScript>().buttonPress(Direction);
         Monitor2.GetComponent<MonitorScript>().buttonPress(Direction);
         Monitor3.GetComponent<MonitorScript>().buttonPress(Direction);
