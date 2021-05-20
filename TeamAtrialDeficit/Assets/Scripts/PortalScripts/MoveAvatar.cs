15fb4a288eb182f213708924fd0e6e0974db9905
Brendan <48538260+ThatFrog@users.noreply.github.com>
2021-05-20 12:13:19 +1000

    Multiplayer fix

--- TeamAtrialDeficit/Assets/Scripts/PortalScripts/MoveAvatar.cs
+++ TeamAtrialDeficit/Assets/Scripts/PortalScripts/MoveAvatar.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class MoveAvatar : MonoBehaviour
 {
     public float movementSpeed = 0.2f;
     public float rotationSpeed = 0.2f;
+    private PhotonView photonView;
+    public GameObject myCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        photonView = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        transform.position += v * transform.forward * Time.deltaTime * 100.0f * movementSpeed;
-        transform.rotation *= Quaternion.AngleAxis(h * 1000.0f * Time.deltaTime * rotationSpeed, transform.up);
+        if (photonView.IsMine == true)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            transform.position += v * transform.forward * Time.deltaTime * 100.0f * movementSpeed;
+            transform.rotation *= Quaternion.AngleAxis(h * 1000.0f * Time.deltaTime * rotationSpeed, transform.up);
+        }
+        else
+        {
+            myCamera.SetActive(false); //disables camera if not player avatar
+            this.transform.GetChild(2).gameObject.SetActive(false); //disable portalview camera if not player avatar
+            this.transform.GetChild(3).gameObject.SetActive(false); //disable portalview camera if not player avatar
+            this.transform.GetChild(4).gameObject.SetActive(false); //disable portalview camera if not player avatar
+            this.transform.GetChild(5).gameObject.SetActive(false); //disable portalview camera if not player avatar
+        }
     }
 }
