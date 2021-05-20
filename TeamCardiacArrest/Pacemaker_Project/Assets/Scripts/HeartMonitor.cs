fa60b28af434a72851f9b5183639f554bb59d87e
Phillip G <phillip.georgoudakis@gmail.com>
2021-05-21 01:21:18 +1000

    - Edited UI to include accurate room selection (The UI is placed under the Canvas, not connected yet) - Added final touches to heartbeat code, introducing difficulty and accurate heartbeat intrevals

--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/HeartMonitor.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/HeartMonitor.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 
 public class HeartMonitor : MonoBehaviour
 {
-    public SoundManager sound;    
+    //public SoundManager sound;    
+    
+    
     //List of spectrum values
     private List<double> timeSeries = new List<double>();
     private List<double> savedSeries = new List<double>();
     //Number of sensor readings
-    private int numReading = 256;
+    private int numReading = 128;
 
     private float width = 6.0f; //Width of spectrum chart
-    private float timeOffset = 0.0f;  //Offset t o center of the spectrum chart
+    private float timeOffset = 0.0f;  //Offset to center of the spectrum chart
 
     private int timeStep = 4; //Number of adjacent bins that are condensed into one
     public float smoothedPeak = 0.0f; //Smoothed value for the peak measured at each frame
@@ -22,7 +24,16 @@ public class HeartMonitor : MonoBehaviour
     private float minFreq = 0.50f;
     private float maxFreq = 3.90f;
 
+    //editable
     public float BPM = 60f;
+    public float Apower = 3.9f;
+    public float Vpower = 2.1f;
+    public string difficulty;
+
+    //saved
+    public float savedBPM;
+    public float savedApower;
+    public float savedVpower;
 
     public GameObject timeSeriesParent;
     public GameObject frequencySeriesParent;
@@ -36,10 +47,6 @@ public class HeartMonitor : MonoBehaviour
     private bool enter;
     private bool playRecording;
 
-    void CreateBeat()
-	{
-	}
-
     private void drawChart(List<double> values, GameObject parent)
     {
         //Clear spectrum chart
@@ -47,36 +54,35 @@ public class HeartMonitor : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
-        for (int i = 0; i < values.Count / 2; i+= timeStep)
-        {
-            //Spectrum width (How long it is)
-            GameObject g = Instantiate(barTemplate);
+          for (int i = 0; i < values.Count; i ++)
+            {
+                //Spectrum width (How long it is)
+                GameObject g = Instantiate(barTemplate);
             g.transform.position = new Vector3(width * (float) i / (float) (numReading / 2) + timeOffset, 0, 0);
             g.transform.localScale = new Vector3(width / (float) (numReading / (2 * timeStep)), 
                                                 0.5f * (float) values[i],
                                                 width / (float)(numReading / (2 * timeStep)));
             g.transform.SetParent(parent.transform, false);
         }
+        beat();
     }
 
     private void drawChart()
 	{
-        drawChart(savedSeries, timeSeriesParent);
+    drawChart(savedSeries, timeSeriesParent);
 	}
 
     //Saves current view of the frequency for later use.
     public void RecordHeartbeat()
-	{
+    {
         debugText.text = "Current heartrate view saved";
         toggleButton.interactable = true;
 
 
-        for (int i = 0; i < numReading; i++)
-		{
-            savedSeries[i] = timeSeries[i];
-		}
+        savedBPM = BPM;
+        savedApower = Apower;
+        savedVpower = Vpower;
 
-        drawChart(savedSeries, frequencySeriesParent);
     }
 
     //Toggle the playRecording boolean and (change the play button text )
@@ -91,10 +97,9 @@ public class HeartMonitor : MonoBehaviour
             toggleText.text = "Stop";
             recordButton.interactable = false;
 
-            for (int i = 0; i < numReading; i++)
-            {
-                savedSeries[i] = timeSeries[i];
-            }
+            BPM = savedBPM;
+            Apower = savedApower;
+            Vpower = savedVpower;
         }
         else
         { 
@@ -123,7 +128,19 @@ public class HeartMonitor : MonoBehaviour
     {
         //Sample acceleration
         Vector3 acc = Input.acceleration;
+
         timeSeries.Add(acc.magnitude);
+        /*
+        switch (difficulty)
+        {
+            case "easy":
+                timeSeries.Add(0.5f);
+                break;
+            case "hard":
+                break;            
+        }
+        */
+
 
         while (timeSeries.Count > numReading)
         {
@@ -133,7 +150,6 @@ public class HeartMonitor : MonoBehaviour
         {
             if (!playRecording)
             {
-
                 double[] seriesReal = new double[numReading];
                 double[] seriesComplex = new double[numReading];
                 for (int i = 0; i < numReading; i++)
@@ -156,7 +172,7 @@ public class HeartMonitor : MonoBehaviour
                 bool maxFound = false;
                 float peakFreq = 0.0f;
 
-                for (int i = 1; i < numReading / 2; i += timeStep)
+                for (int i = 1; i < numReading / 2 ; i += timeStep)
 				{
                     for (int j = 0; j < timeStep; j++)
 					{
@@ -170,11 +186,11 @@ public class HeartMonitor : MonoBehaviour
                                 maxLevel = v;
                                 maxFound = true;
                                 peakFreq = freq;
+
 							}
 						}
                     }
                 }
-                //BPM = (int)Mathf.RoundToInt(smoothedPeak * 60); // Calculate BPM (Beats per second * 60 seconds)
                 smoothedPeak = smoothingFactor * smoothedPeak + (1.0f - smoothingFactor) * peakFreq;
              
                 drawChart(timeSeries, timeSeriesParent);
@@ -223,28 +239,24 @@ public class HeartMonitor : MonoBehaviour
 
 
     //Frequency
-    [SerializeField] private float Hertz;
-    [SerializeField] private float PeriodT; //1/FrequencyHertz
-    [SerializeField] private float remainPeriod;
-    [SerializeField] private float remainPeriodMillisec;
+    private float Hertz;
+    private float PeriodT; //FrequencyHertz
+    private float remainPeriod;
+    private float remainPeriodMillisec;
     public float returnTime = .01f;
-    [SerializeField] private float startReturnTime;
-    [SerializeField] private float startReturnTimeMillisec;
+    private float startReturnTime;
+    private float startReturnTimeMillisec;
 
     //State
-    public float[] GlobalTimer = { 0, 0 };
-    public float[] CatchGlobalTimer = { 0, 0 };
-    [SerializeField] private bool Lub;
     public int stateIndex = 0;
     public int isBeating;
-
-    //Limbs
-    public Toggle debugToggle;
+    private bool Lub;
     
     public void beat()
 	{
         Hertz = BPM / 60f;
         PeriodT = 1 / Hertz; //https://www.quora.com/How-do-you-convert-Hertz-to-seconds
+
         if (!Lub)
         {
             remainPeriod -= Time.deltaTime;
@@ -254,18 +266,8 @@ public class HeartMonitor : MonoBehaviour
             if (remainPeriodMillisec <= 0f)
             {
                 stateIndex = 1;
-                if (debugToggle) debugToggle.isOn = true;
-                timeSeries.Add(3.9f);
-                sound.beep();
-                /*
-                if (Systole.Length > 0)
-                {
-                    for (int i = 0; i < Systole.Length; i++)
-                    {
-                        Speaker.PlayOneShot(Systole[i]);
-                    }
-                }
-                */
+                timeSeries.Add(Apower);    //<----
+                //sound.beep();
                 Lub = true;
             }
         }
@@ -275,20 +277,10 @@ public class HeartMonitor : MonoBehaviour
             remainPeriodMillisec = PeriodT * 1000;
             startReturnTime -= Time.deltaTime;
             startReturnTimeMillisec -= Time.deltaTime * 1000;
-            if (startReturnTimeMillisec <= 0f)
+            if (startReturnTimeMillisec <= Time.deltaTime / 2)
             {
                 stateIndex = 0;
-                if (debugToggle) debugToggle.isOn = false;
-                
-                /*
-                if (Diastole.Length > 0)
-                {
-                    for (int i = 0; i < Diastole.Length; i++)
-                    {
-                        Speaker.PlayOneShot(Diastole[i]);
-                    }
-                }
-                */
+                timeSeries.Add(Vpower);   //<-
                 Lub = false;
             }
         }
