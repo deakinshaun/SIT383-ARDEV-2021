76784bc7261d763e6837de28c3945b24fc7964c5
Aya <64139634+Pixelmixery@users.noreply.github.com>
2021-05-22 21:34:39 +1000

    Update RoomManager.cs

--- TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/RoomManager.cs
+++ TeamCardiacArrest/Pacemaker_Project/Assets/Scripts/RoomManager.cs
@@ -131,15 +131,15 @@ public class RoomManager : MonoBehaviourPunCallbacks
         switch (ddGroup.value)
         {
             case 0:
-            tex = LobbyRoom;
-            break;
-
-            case 1:
             tex = SoloRoom;
             break;
 
-            case 2:
+            case 1:
             tex = GroupRoom;
+            break;
+
+            case 2:
+            tex = LobbyRoom;
             break;
             
             default:
@@ -235,7 +235,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.LeaveRoom();
         } 
-        else if (ddGroup.value == 0)
+        else if (ddGroup.value == 0 && ddType.value ==1)
         {
             GameObject avatar = new GameObject(); 
             avatar = PhotonNetwork.Instantiate(SetupPrefab.name, new Vector3(0.0f, ((float)PhotonNetwork.CurrentRoom.PlayerCount*0.1f), 0.0f), Quaternion.identity, 0);
@@ -270,6 +270,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     {
         soundsMan.EndSimulation();
         PhotonNetwork.LeaveRoom();
+        ddGroup.value = 2;
         RoomSelect.SetActive(true);
         RoomCanvas.SetActive(false);               
     }
